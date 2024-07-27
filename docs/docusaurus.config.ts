334e688bd56fc6f4dff001b4390d25418aa38dcb
prulloac <pablo.ulloac@usach.cl>
2024-07-27 03:17:28 +0000

    docs: updated docs config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -3,15 +3,15 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'FinEasy',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://prulloac.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/fineasy',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
